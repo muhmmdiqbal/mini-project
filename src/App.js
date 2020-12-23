1a71bacb5704875b906b42b08548a318574d4b9b
muhmmqibal <muhammadiqbal130996@gmail.com>
2020-12-23 21:52:39 +0700

    test awal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+// import Overview from './components/Overview'
+import Detailpage from './Detailpage'
+
 
 function App() {
   return (
     <>
       <Header />
-      
+      {/* <Overview /> */}
+      <Detailpage />
       <Footer />
     </>
   );
---
+++ src/Overview/Overview.js
@@ -0,0 +1,66 @@
+import React, {Component, Fragment } from 'react'
+import {  Container, } from 'react-bootstrap'
+import Synopsis from './../components/Synopsis/Synopsis'
+import axios from 'axios'
+
+class Overview extends Component {
+    state = {
+        synopsis: []
+    }
+    
+    componentDidMount(){
+        // fetch('https://jsonplaceholder.typicode.com/posts/1')
+        
+        // .then((response) => response.json())
+        // .then(json => {
+        //     this.setState({
+        //         synopsis: json
+        //     })
+        // })
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        
+        .then((result) => {
+            // console.log(result.data);
+            this.setState({
+                synopsis: result.data
+            })
+            
+        })
+        
+        
+    }
+    render() {
+    return (
+        <Fragment>
+       <div> 
+       
+        <Container >
+        
+        {
+            this.state.synopsis.map(synopsis => {
+                return <Synopsis key={synopsis.id} title={synopsis.title} body={synopsis.body}/>
+            })
+            
+        }
+        
+        {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.</p>       
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.</p> */}
+        <br/>
+        <h4><hr/></h4>
+        
+        {/* <p><strong>Release date: </strong>January 5, 1998</p>
+        <p><strong>Director: </strong>John Doe</p>
+        <p><strong>Featured Song: </strong>Pegasus fantasi</p>
+        <p><strong>Budget: </strong>200 million USD</p>
+        <p><strong>Release date: </strong>January 5, 1998</p>
+        <p><strong>Director: </strong>John Doe</p>
+        <p><strong>Featured Song: </strong>Pegasus fantasi</p>
+        <p><strong>Budget: </strong>200 million USD</p> */}
+        </Container>
+        
+        </div>
+        </Fragment>
+    )
+}}
+
+export default Overview
